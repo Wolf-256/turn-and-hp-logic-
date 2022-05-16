73ea1a7e0a8539dc932a43173e4057887ff942c0
Wolf-256 <93338953+Wolf-256@users.noreply.github.com>
2022-05-16 12:14:14 -0400

    Create turns-and-health.cpp

---
+++ turns-and-health.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+int main() {
+
+  string alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  string alphabetArray[26];
+  for (int i = 0; i < alphabetString.length(); i++){
+  alphabetArray[i]=alphabetString[i];
+  }
+  int alphabetSize = alphabetString.length();
+  string userInputs[alphabetSize];
+  int turnNumber = 0;
+  int healthPoints = 99;
+  
+  
+  cout << alphabetSize << "\n";
+  cout << "first letter of array is  " << alphabetArray[0] << "\n";
+  cout << "last letter of array is  " << alphabetArray[25] << "\n";
+  cout << "TURN:  "<< turnNumber << "\n";
+  cout << "TURN:  "<< healthPoints << "\n";
+  
+  do{
+        
+  cout << "game" << "\n";
+  cout << "TURN:  "<< turnNumber << "\n";
+  cout << "TURN:  "<< healthPoints << "\n";
+  turnNumber++;
+  healthPoints -= 9;
+  }while (healthPoints>0);
+  
+    return 0;
+}
